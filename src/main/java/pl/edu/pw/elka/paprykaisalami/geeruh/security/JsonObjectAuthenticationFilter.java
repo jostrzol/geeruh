b4be2c8674c0c9a96705b007d8ae58c3aeffb847
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2022-11-12 21:18:42 +0100

    GEE-31 | rename login request

--- src/main/java/pl/edu/pw/elka/paprykaisalami/geeruh/security/JsonObjectAuthenticationFilter.java
+++ src/main/java/pl/edu/pw/elka/paprykaisalami/geeruh/security/JsonObjectAuthenticationFilter.java
@@ -21,7 +21,7 @@ public class JsonObjectAuthenticationFilter extends UsernamePasswordAuthenticati
         try {
             BufferedReader reader = request.getReader();
             String body = reader.lines().collect(Collectors.joining());
-            LoginCredentialsDto authRequest = objectMapper.readValue(body, LoginCredentialsDto.class);
+            LoginRequest authRequest = objectMapper.readValue(body, LoginRequest.class);
             UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
                     authRequest.getUsername(), authRequest.getPassword()
             );
