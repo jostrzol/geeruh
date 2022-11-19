d0c48e686ae2816bbf13354054f668e58a0138ec
Igor Anczykowski <i.anczykowski@stud.elka.pw.edu.pl>
2022-11-19 14:19:44 +0100

    GEE-32 Przywrócenie AppTests

--- src/test/java/pl/edu/pw/elka/paprykaisalami/geeruh/GeeruhApplicationTests.java
+++ src/test/java/pl/edu/pw/elka/paprykaisalami/geeruh/GeeruhApplicationTests.java
@@ -1,19 +1,13 @@
 package pl.edu.pw.elka.paprykaisalami.geeruh;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import pl.edu.pw.elka.paprykaisalami.geeruh.repositories.StatusRepository;
 
 @SpringBootTest
 class GeeruhApplicationTests {
 
-    @Autowired
-    StatusRepository statusRepository;
-
     @Test
     void contextLoads() {
-        statusRepository.findAll();
     }
 
 }
