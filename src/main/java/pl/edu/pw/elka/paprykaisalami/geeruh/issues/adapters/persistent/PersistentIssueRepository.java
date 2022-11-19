e25e48e08d5da68f815f7132bbf37172168e58b4
Jakub Ostrzołek <jakub.ostrzolek.stud@pw.edu.pl>
2022-11-19 17:36:00 +0100

    GEE-32 | Suggest changes to PersistentIssueRepository

--- src/main/java/pl/edu/pw/elka/paprykaisalami/geeruh/issues/adapters/persistent/PersistentIssueRepository.java
+++ src/main/java/pl/edu/pw/elka/paprykaisalami/geeruh/issues/adapters/persistent/PersistentIssueRepository.java
@@ -1,36 +1,29 @@
 package pl.edu.pw.elka.paprykaisalami.geeruh.issues.adapters.persistent;
 
+import lombok.AllArgsConstructor;
 import org.jetbrains.annotations.Nullable;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-import pl.edu.pw.elka.paprykaisalami.geeruh.issues.domain.models.Description;
-import pl.edu.pw.elka.paprykaisalami.geeruh.issues.domain.models.Issue;
-import pl.edu.pw.elka.paprykaisalami.geeruh.issues.domain.models.IssueId;
-import pl.edu.pw.elka.paprykaisalami.geeruh.issues.domain.models.IssueType;
-import pl.edu.pw.elka.paprykaisalami.geeruh.issues.domain.models.Summary;
+import pl.edu.pw.elka.paprykaisalami.geeruh.issues.domain.models.*;
 import pl.edu.pw.elka.paprykaisalami.geeruh.issues.domain.ports.IssueRepository;
 
-import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
+@AllArgsConstructor
 @Primary
 @Component
 public class PersistentIssueRepository implements IssueRepository {
 
-    final
-    EntityManager entityManager;
-
-    public PersistentIssueRepository(EntityManager entityManager) {
-        this.entityManager = entityManager;
-    }
+    ActualPersistentIssueRepository actualRepository;
 
     @Override
     public List<Issue> findAll() {
-        return entityManager.createQuery("from IssuePersistent", IssuePersistent.class)
-                .getResultList()
+        return actualRepository.findAll()
                 .stream()
                 .map(IssuePersistent::toIssue)
                 .collect(Collectors.toList());
@@ -38,18 +31,19 @@ public class PersistentIssueRepository implements IssueRepository {
 
     @Override
     public Optional<Issue> findById(IssueId issueId) {
-        var ip = entityManager.find(IssuePersistent.class, issueId.getValue());
-        if (ip == null) {
-            return Optional.empty();
-        }
-        return Optional.ofNullable(ip.toIssue());
+        return actualRepository.findById(issueId.getValue())
+                .map(IssuePersistent::toIssue);
     }
 
     @Transactional
     @Override
     public Issue save(IssueType type, Summary summary, @Nullable Description description) {
         var issuePersistent = new IssuePersistent(type, summary, description);
-        entityManager.persist(issuePersistent);
-        return issuePersistent.toIssue();
+        return actualRepository.save(issuePersistent)
+                .toIssue();
     }
+}
+
+@Component
+interface ActualPersistentIssueRepository extends JpaRepository<IssuePersistent, UUID> {
 }
