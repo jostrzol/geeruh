f932d66664c9643c2de536378e8487c4c0314440
Igor Anczykowski <i.anczykowski@stud.elka.pw.edu.pl>
2022-11-19 21:43:51 +0100

    GEE-32 | Removed old model

--- src/main/java/pl/edu/pw/elka/paprykaisalami/geeruh/repositories/StatusRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.edu.pw.elka.paprykaisalami.geeruh.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.edu.pw.elka.paprykaisalami.geeruh.models.Status;
-
-public interface StatusRepository extends JpaRepository<Status, Integer> {
-}
