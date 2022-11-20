741197f33ce00e40b6bec90213c2d8061c14b1b9
Igor Anczykowski <i.anczykowski@stud.elka.pw.edu.pl>
2022-11-20 13:25:10 +0100

    GEE-32 | Fixed HistoryType enum after moving to spring-data-envers

--- src/main/java/pl/edu/pw/elka/paprykaisalami/geeruh/issues/domain/models/IssueHistoryEntryType.java
+++ src/main/java/pl/edu/pw/elka/paprykaisalami/geeruh/issues/domain/models/IssueHistoryEntryType.java
@@ -1,8 +1,9 @@
 package pl.edu.pw.elka.paprykaisalami.geeruh.issues.domain.models;
 
 public enum IssueHistoryEntryType {
-    ADD,
-    MOD,
-    DEL
+    UNKNOWN,
+    INSERT,
+    UPDATE,
+    DELETE
 }
 
