6fbeb39d49518abd6f5c36cc25f8cee3987be593
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2022-12-07 18:34:14 +0100

    GEE-58 | renames

--- src/integrationTest/java/pl/edu/pw/elka/paprykaisalami/geeruh/support/IssueDataset.java
+++ src/integrationTest/java/pl/edu/pw/elka/paprykaisalami/geeruh/support/IssueDataset.java
@@ -2,32 +2,30 @@ package pl.edu.pw.elka.paprykaisalami.geeruh.support;
 
 import org.json.JSONObject;
 
-import java.util.UUID;
-
 public class IssueDataset {
 
-    public static final String FIRST_SUMMARY = "First issue";
-    public static final String FIRST_TYPE = "TASK";
-    public static final String FIRST_DESCRIPTION = "Important issue";
+    public static final String FIRST_ISSUE_SUMMARY = "First issue";
+    public static final String FIRST_ISSUE_TYPE = "TASK";
+    public static final String FIRST_ISSUE_DESCRIPTION = "Important issue";
     public static final JSONObject FIRST_ISSUE = new JSONObject()
-            .put("summary", FIRST_SUMMARY)
-            .put("type", FIRST_TYPE)
-            .put("description", FIRST_DESCRIPTION);
+            .put("summary", FIRST_ISSUE_SUMMARY)
+            .put("type", FIRST_ISSUE_TYPE)
+            .put("description", FIRST_ISSUE_DESCRIPTION);
     public static final String FIRST_ISSUE_STRING = FIRST_ISSUE.toString();
 
-    public static final String SECOND_SUMMARY = "Second issue";
-    public static final String SECOND_TYPE = "BUG";
-    public static final String SECOND_DESCRIPTION = "Even more important issue";
+    public static final String SECOND_ISSUE_SUMMARY = "Second issue";
+    public static final String SECOND_ISSUE_TYPE = "BUG";
+    public static final String SECOND_ISSUE_DESCRIPTION = "Even more important issue";
     public static final JSONObject SECOND_ISSUE = new JSONObject()
-            .put("summary", SECOND_SUMMARY)
-            .put("type", SECOND_TYPE)
-            .put("description", SECOND_DESCRIPTION);
+            .put("summary", SECOND_ISSUE_SUMMARY)
+            .put("type", SECOND_ISSUE_TYPE)
+            .put("description", SECOND_ISSUE_DESCRIPTION);
     public static final String SECOND_ISSUE_STRING = SECOND_ISSUE.toString();
 
-    public static final String THIRD_SUMMARY = "Third Issue";
-    public static final String THIRD_TYPE = "TASK";
+    public static final String THIRD_ISSUE_SUMMARY = "Third Issue";
+    public static final String THIRD_ISSUE_TYPE = "TASK";
     public static final JSONObject THIRD_ISSUE_NO_DESCRIPTION = new JSONObject()
-            .put("summary", THIRD_SUMMARY)
-            .put("type", THIRD_TYPE);
+            .put("summary", THIRD_ISSUE_SUMMARY)
+            .put("type", THIRD_ISSUE_TYPE);
     public static final String THIRD_ISSUE_NO_DESCRIPTION_STRING = THIRD_ISSUE_NO_DESCRIPTION.toString();
 }
---
+++ src/integrationTest/java/pl/edu/pw/elka/paprykaisalami/geeruh/support/ProjectDataset.java
@@ -0,0 +1,33 @@
+package pl.edu.pw.elka.paprykaisalami.geeruh.support;
+
+import org.json.JSONObject;
+
+import java.util.UUID;
+
+public class IssueDataset {
+
+    public static final String FIRST_SUMMARY = "First issue";
+    public static final String FIRST_TYPE = "TASK";
+    public static final String FIRST_DESCRIPTION = "Important issue";
+    public static final JSONObject FIRST_ISSUE = new JSONObject()
+            .put("summary", FIRST_SUMMARY)
+            .put("type", FIRST_TYPE)
+            .put("description", FIRST_DESCRIPTION);
+    public static final String FIRST_ISSUE_STRING = FIRST_ISSUE.toString();
+
+    public static final String SECOND_SUMMARY = "Second issue";
+    public static final String SECOND_TYPE = "BUG";
+    public static final String SECOND_DESCRIPTION = "Even more important issue";
+    public static final JSONObject SECOND_ISSUE = new JSONObject()
+            .put("summary", SECOND_SUMMARY)
+            .put("type", SECOND_TYPE)
+            .put("description", SECOND_DESCRIPTION);
+    public static final String SECOND_ISSUE_STRING = SECOND_ISSUE.toString();
+
+    public static final String THIRD_SUMMARY = "Third Issue";
+    public static final String THIRD_TYPE = "TASK";
+    public static final JSONObject THIRD_ISSUE_NO_DESCRIPTION = new JSONObject()
+            .put("summary", THIRD_SUMMARY)
+            .put("type", THIRD_TYPE);
+    public static final String THIRD_ISSUE_NO_DESCRIPTION_STRING = THIRD_ISSUE_NO_DESCRIPTION.toString();
+}
