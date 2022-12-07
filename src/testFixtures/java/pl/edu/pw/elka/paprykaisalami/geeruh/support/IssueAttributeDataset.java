2f267b85016416a7197a0a92dbf422dd9e35f65c
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2022-12-07 21:11:22 +0100

    GEE-80 | split attributes from datasets

---
+++ src/testFixtures/java/pl/edu/pw/elka/paprykaisalami/geeruh/support/IssueAttributeDataset.java
@@ -0,0 +1,15 @@
+package pl.edu.pw.elka.paprykaisalami.geeruh.support;
+
+public class IssueAttributeDataset {
+
+    public static final String FIRST_ISSUE_SUMMARY = "First issue";
+    public static final String FIRST_ISSUE_TYPE = "TASK";
+    public static final String FIRST_ISSUE_DESCRIPTION = "Important issue";
+
+    public static final String SECOND_ISSUE_SUMMARY = "Second issue";
+    public static final String SECOND_ISSUE_TYPE = "BUG";
+    public static final String SECOND_ISSUE_DESCRIPTION = "Even more important issue";
+
+    public static final String THIRD_ISSUE_SUMMARY = "Third Issue";
+    public static final String THIRD_ISSUE_TYPE = "TASK";
+}
---
+++ src/testFixtures/java/pl/edu/pw/elka/paprykaisalami/geeruh/support/ProjectAttributeDataset.java
@@ -0,0 +1,15 @@
+package pl.edu.pw.elka.paprykaisalami.geeruh.support;
+
+public class ProjectAttributeDataset {
+
+    public static final String FIRST_PROJECT_CODE = "FST";
+    public static final String FIRST_PROJECT_NAME = "First project";
+    public static final String FIRST_PROJECT_DESCRIPTION = "Important project";
+
+    public static final String SECOND_PROJECT_CODE = "SND";
+    public static final String SECOND_PROJECT_NAME = "Second project";
+    public static final String SECOND_PROJECT_DESCRIPTION = "Not so important project";
+
+    public static final String THIRD_PROJECT_CODE = "THD";
+    public static final String THIRD_PROJECT_NAME = "Third project";
+}
